950b9e85b07e0a1e1b9b1f63adece7fbc21daa8c
Muhammad Usama <usamamuhammad100@gmail.com>
2020-02-05 20:41:16 +0500

    formating

--- lib/Screens/CourseAddDrop/course_add_drop_screen.dart
+++ lib/Screens/CourseAddDrop/course_add_drop_screen.dart
@@ -8,45 +8,55 @@ class CourseAddDrop extends StatefulWidget {
 }
 
 class _CourseAddDropState extends State<CourseAddDrop> {
-  int flexSearch=20;
-  int flexCourseList=80;
+  int flexSearch = 20;
+  int flexCourseList = 80;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new SearchBar(
+        loader: QuerySetLoader(
+          querySetCall: null,
+          itemBuilder: null,
+          loadOnEachChange: true,
+          animateChanges: true,
+        ),
         attrs: SearchBarAttrs(),
 //      overlayStyle: SystemUiOverlayStyle(),
-      defaultBar: AppBar(elevation: 0,backgroundColor: Colors.blue,),
+        defaultBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.blue,
+        ),
       ),
       body: Container(
         child: Column(
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Flexible(flex: flexSearch,
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: new Container(
-                    color: Colors.red,
-                    child: new Text("jkkkkl"),
+            Flexible(
+              flex: flexSearch,
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    child: new Container(
+                      color: Colors.red,
+                      child: new Text("jkkkkl"),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-            ),
-            Flexible(flex: flexCourseList,
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    color: Colors.blue,
-                    child: new Text("Pol"),
+            Flexible(
+              flex: flexCourseList,
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                      child: new Text("Pol"),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-            ),
-
           ],
         ),
       ),

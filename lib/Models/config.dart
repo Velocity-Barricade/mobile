feb6bfd98768fd67585f5120bfaa11305060e3b8
Saad Ismail <saad3112@gmail.com>
2020-02-05 16:29:49 +0500

    fix remote config; implement getClasses

--- lib/Models/config.dart
+++ lib/Models/config.dart
@@ -1,14 +1,17 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+
 class Config {
-  static String baseUrl = "https://timetablenotifier.com:3000";
+  static String baseUrl = "http://52.200.188.3:3000";
+//  static String baseUrl = "http://timetablenotifier.com:3000";
   static String updateCourseRoute = "/user/updateCourses";
   static String getUserClassesRoute = "/user/getClasses/:email";
   static String getCompleteTimetableRoute = "/user/getCompleteTimetable";
 
-  static fromJson(Map<String, dynamic> json) {
-    baseUrl = json['base_url'].toString();
-    updateCourseRoute = json['update_course_route'].toString();
-    getUserClassesRoute = json['get_user_classes_route'].toString();
-    getCompleteTimetableRoute = json['get_complete_timetable_route'].toString();
+  static fromJson(Map<String, RemoteConfigValue> json) {
+    baseUrl = json['base_url'].asString();
+    updateCourseRoute = json['update_course_route'].asString();
+    getUserClassesRoute = json['get_user_classes_route'].asString();
+    getCompleteTimetableRoute = json['get_complete_timetable_route'].asString();
   }
 
   static Map<String, dynamic> toJson() => {

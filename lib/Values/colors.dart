d110c63bb6282579c0982792825ffeeb2c876ab3
shakeeb1998 <shakeebsiddiqui1998@gmail.com>
2020-02-05 15:52:43 +0500

    TimeTable Ui Change

--- lib/Values/colors.dart
+++ lib/Values/colors.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
 
-Color themeColor= Color.fromRGBO(38,178,211,1);
+Color themeColor= Color.fromRGBO(38,178,210,1);
+Color themeSecondary = Color.fromRGBO(109,213,237,1);
 Color themeGrey= Color.fromRGBO(0,0,0,0.5);

e8567e2a272bad42fbd4fc18b11cd0d8536de268
Saad Ismail <saad3112@gmail.com>
2020-02-07 12:07:51 +0500

    persist config to sharedpreferences; add parsedTimetable

--- lib/Utils/get_store.dart
+++ lib/Utils/get_store.dart
@@ -1,20 +1,18 @@
-
 import 'package:barricade/Utils/local_storage_handler.dart';
 import 'package:barricade/Utils/request_manager.dart';
 
-get_store_timetable() async {
-
+getStoreTimetable() async {
   print("hello");
-  var classMap = await RequestManager.getClasses(email: "shakeebsiddiqui1998@gmail.com");
+  var classMap =
+      await RequestManager.getClasses(email: "shakeebsiddiqui1998@gmail.com");
   print('HERE');
-  StorageHandler().storeClasses(timeTable: classMap,email: "shakeebsiddiqui1998@gmail.com");
-
+  StorageHandler().storeClasses(
+      timeTable: classMap, email: "shakeebsiddiqui1998@gmail.com");
 }
 
-get_timetable({String email}){
-
-  var timetable=StorageHandler().getClasses(email: "shakeebsiddiqui1998@gmail.com");
+getTimetable({String email}) {
+  var timetable =
+      StorageHandler().getClasses(email: "shakeebsiddiqui1998@gmail.com");
 
   return timetable;
-
 }

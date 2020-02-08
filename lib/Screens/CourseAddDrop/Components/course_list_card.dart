f38f925759f4680b64231987911ae487966294c8
Muhammad Usama <usamamuhammad100@gmail.com>
2020-02-08 13:11:22 +0500

    color change

--- lib/Screens/CourseAddDrop/Components/course_list_card.dart
+++ lib/Screens/CourseAddDrop/Components/course_list_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:barricade/Screens/CourseAddDrop/course_add_drop_screen.dart';
+import "package:barricade/Values/colors.dart";
 
 class CourseListCard extends StatefulWidget {
   var course;
@@ -15,23 +16,28 @@ class _CourseListCardState extends State<CourseListCard> {
     // TODO: implement build
     return Container(
       height: 70,
-      child: ListTile(
-        title: Text(
-          widget.course.name,
-          style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.8),
-        ),
-        trailing: Checkbox(
-          value: selectvalue,
-          onChanged: (bool value) {
-            if (!value && selectvalue)
-              ListSelected.selected.remove(widget.course);
-            else if (value && !selectvalue)
-              ListSelected.selected.add(widget.course);
-            print(ListSelected.selected);
-            setState(() {
-              selectvalue = value;
-            });
-          },
+      child: Card(
+        color: themeSecondary,
+        child: ListTile(
+          title: Text(
+            widget.course.name,
+            style: DefaultTextStyle.of(context)
+                .style
+                .apply(fontSizeFactor: 1.8, color: Colors.white),
+          ),
+          trailing: Checkbox(
+            value: selectvalue,
+            onChanged: (bool value) {
+              if (!value && selectvalue)
+                ListSelected.selected.remove(widget.course);
+              else if (value && !selectvalue)
+                ListSelected.selected.add(widget.course);
+              print(ListSelected.selected);
+              setState(() {
+                selectvalue = value;
+              });
+            },
+          ),
         ),
       ),
     );
--- lib/Values/colors.dart
+++ lib/Values/colors.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/cupertino.dart';
 
-Color themeColor= Color.fromRGBO(38,178,210,1);
-Color themeSecondary = Color.fromRGBO(109,213,237,1);
-Color themeGrey= Color.fromRGBO(0,0,0,0.5);
+Color themeColor = Color.fromRGBO(38, 178, 210, 1);
+Color themeSecondary = Color.fromRGBO(109, 213, 237, 1);
+Color themeGrey = Color.fromRGBO(0, 0, 0, 0.5);
